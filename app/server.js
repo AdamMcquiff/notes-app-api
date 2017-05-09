9daaea091743174bc0cb4b696614af60fc820f0a
Adam McKenna <adam@gospelware.co.uk>
2017-05-09 12:40:24 +0100

    Added Model / DB Connection

--- app/server.js
+++ app/server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Note = require('./app/models/note');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = 8080;
+var port = 3300;
 var router = express.Router();
+
+mongoose.connect('mongodb://127.0.0.1:27017'); // connect to our database
 
 app.use('/api', router);
 
